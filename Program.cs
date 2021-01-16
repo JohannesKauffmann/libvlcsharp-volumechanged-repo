ba781dffc2fba5f6a9e4bea06fb408327cc18830
JohannesKauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-01-16 22:29:15 +0100

    Added volume callback counter, removed timer

--- Program.cs
+++ Program.cs
@@ -1,51 +1,41 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Timers;
 using LibVLCSharp.Shared;
 
-namespace LibVLCSharp.NetCore.Sample
+namespace libvlcsharpvolumechangedrepo
 {
     class Program
     {
-        static readonly Timer timer = new Timer(1 * 1000) { AutoReset = false };
+        static int volume_cb_count = 0;
         static MediaPlayer mp;
 
         static void Main(string[] args)
         {
-            timer.Elapsed += Timer_Elapsed;
-
             Core.Initialize();
 
-            using var libVLC = new LibVLC(enableDebugLogs: true);
+            using var libVLC = new LibVLC(enableDebugLogs: false);
             using var media = new Media(libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
             mp = new MediaPlayer(media);
             mp.Playing += Mp_Playing; ;
             mp.VolumeChanged += Mp_VolumeChanged;
             mp.Play();
             Console.ReadKey();
+            Console.WriteLine(String.Format("Volume callback fired {0} times!", volume_cb_count));
         }
 
         static async void Mp_Playing(object sender, EventArgs e)
         {
             await Task.Run(() =>
             {
-                mp.Volume = 50;
+                mp.Volume = 100;
             });
-            timer.Start();
         }
 
         static void Mp_VolumeChanged(object sender, MediaPlayerVolumeChangedEventArgs e)
         {
-            Debug.WriteLine(String.Format("New volume: {0}", e.Volume));
-        }
-
-        static async void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            await Task.Run(() =>
-            {
-                mp.Volume = 100;
-            });
+            volume_cb_count++;
+            Console.WriteLine(String.Format("New volume: {0}", e.Volume));
         }
     }
 }
